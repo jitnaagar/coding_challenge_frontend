7e7eb0d32cc444922cf408098cb6bc33e14273ab
Jitender Naagar <jitendernaagar@Jitenders-MacBook-Pro.local>
2022-04-11 12:14:58 +0400

    coding test

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import Axios from 'axios';
+import './App.css';
+
+function App() {
+  const [search, setSearch] = useState('');
+  const [customers, setCustomers] = useState([]);
+  const [total, setTotal] = useState(0);
+  const [page, setPage] = useState(1); // No of pages
+  const [perPage, setPerPage] = useState(1);
+
+  //const perPage = 1;
+  //const lastPage = page * perPage;
+  //const firstPage = lastPage - perPage;
+  //console.log("lastPage::::::::",lastPage)
+  //console.log("firstPage::::::::",firstPage)
+  //const currentPageDetail = customers.slice(firstPage, lastPage);
+
+  const pageNumber = [];
+  for (let i = 1; i <= Math.ceil(total / perPage); i++) {
+    pageNumber.push(i);
+  }
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const handlePage = (event) => {
+    localStorage.setItem("currentPage", event.target.value);
+    setPage(event.target.value);
+  }
+
+  
+
+  useEffect(() => {
+    const currentPage = localStorage.getItem("currentPage")?localStorage.getItem("currentPage"):page;
+    setPage(currentPage);
+    const fetchCustomers = async () => {
+      const { data } = await Axios.get(
+        "http://localhost:3000/api/customers?search="+search+"&page="+page
+      );
+      setTotal(data.count);
+      setPerPage(data.limit);
+      setCustomers(data.customers);
+    };
+    fetchCustomers();
+  }, [search,page]);
+  return (
+    <div className="App">
+      <h1>Retrieve list of customers</h1>
+      
+      <div className="search"><label htmlFor="search">
+        Search by username:
+        <input id="search" type="text" onChange={handleSearch} />
+      </label>
+      </div>
+      <table>
+    
+    <thead>
+      <tr>
+        <th>Full Name</th>
+        <th>Username</th>
+        <th>email</th>
+        <th>Picture</th>
+      </tr>
+    </thead>
+    <tbody>
+    {customers.map((customer) => (
+    <tr>
+     <td data-label="fullName">{customer.full_name}</td>
+     <td data-label="username">{customer.username}</td>
+     <td data-label="email">{customer.email}</td>
+     <td data-label="picture"><img src={customer.picture} width="50" alt=""/></td>
+    </tr>
+    ))}
+    </tbody>
+  </table>
+  <div className="pagination">
+            {/* <button>
+              Previous
+            </button> */}
+ 
+            {pageNumber.map((Elem) => {
+              return (
+                <>
+                  <button value={Elem} onClick={handlePage}>
+                    {Elem}
+                  </button>
+                </>
+              );
+            })}
+            {/* <button>
+              Next
+            </button> */}
+          </div>
+    </div>
+  );
+}
+
+export default App;
